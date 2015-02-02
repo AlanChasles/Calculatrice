b04a51373999ecb0dad8281ae4b8184e081bf21d
AlanChasles <alan.chasles@gmail.com>
2015-02-02 14:47:39 +0100

    fonctions terminées

--- main.cpp
+++ main.cpp
@@ -5,5 +5,103 @@ using namespace std;
 int main() {
 
 
+//Fonctions additions
+int32_t addition(int32_t opA,int32_t opB){
+	return opA + opB;
+}
+
+int64_t addition(int64_t opA,int64_t opB){
+	return opA + opB;
+}
+
+float addition(float opA, float opB){
+	return opA + opB;
+}
+
+double addition(double opA, double opB){
+	return opA + opB;
+}
+
+
+//---------------------------------------
+
+//Fonctions soustraction
+int32_t soustraction(int32_t opA, int32_t opB){
+	return opA - opB;
+}
+
+int64_t soustraction(int64_t opA, int64_t opB){
+	return opA - opB;
+}
+
+float soustraction(float opA, float opB){
+	return opA - opB;
+}
+
+double soustraction(double opA, double opB){
+	return opA - opB;
+}
+
+
+//---------------------------------------
+
+//Fonctions multiplication
+int32_t multiplication(int32_t opA, int32_t opB){
+	return opA * opB;
+}
+
+int64_t multiplication(int64_t opA, int64_t opB){
+	return opA * opB;
+}
+
+float multiplication(float opA, float opB){
+	return opA * opB;
+}
+
+double multiplication(double opA, double opB){
+	return opA * opB;
+}
+
+
+//---------------------------------------
+
+//Fonctions division
+int32_t division(int32_t opA, int32_t opB){
+	return opA / opB;
+}
+
+int64_t division(int64_t opA, int64_t opB){
+	return opA / opB;
+}
+
+float division(float opA, float opB){
+	return opA / opB;
+}
+
+double division(double opA, double opB){
+	return opA / opB;
+}
+
+
+//---------------------------------------
+
+//Fonctions modulo
+int32_t modulo(int32_t opA, int32_t opB){
+	return opA % opB;
+}
+
+int64_t modulo(int64_t opA, int64_t opB){
+	return opA % opB;
+}
+
+float modulo(float opA, float opB){
+	return opA % opB;
+}
+
+double modulo(double opA, double opB){
+	return opA % opB;
+}
+
+
 return 0;
 }
